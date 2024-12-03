2e3ab7ed33b197a7ccee85b99ee3591ca49113f5
Alyssa Hanson <alyghanson@gmail.com>
2024-12-03 11:07:26 -0800

    pushing to git

---
+++ src/Components/About.tsx
@@ -0,0 +1,13 @@
+// About.js
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Fun project to keep tract of personal plants and their watering schedules.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/Components/Main.tsx
@@ -0,0 +1,13 @@
+// Main App page
+import React from "react";
+
+function Main() {
+  return (
+    <div>
+      <h1>Main Page</h1>
+      <p>Here we will preform most functions</p>
+    </div>
+  );
+}
+
+export default Main;
